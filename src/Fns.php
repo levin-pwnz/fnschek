4e30bcf30edfd191a9cbd594482fc50df327f11f
iglass <vkpasser@yandex.com>
2019-05-17 20:31:48 +0800

    update

--- src/Fns.php
+++ src/Fns.php
@@ -2,12 +2,14 @@
 
 namespace LevinPwnz\FnsCheck;
 
+use FnsCheck\FnsCheckHelper;
 use Illuminate\Support\Collection;
 use LevinPwnz\FnsCheck\Services\FnsService;
 use LevinPwnz\FnsCheck\Services\QrService;
 
 /**
  * Class Fns
+ *
  * @package LevinPwnz\FnsCheck
  */
 class Fns
@@ -24,10 +26,11 @@ class Fns
 
     /**
      * Fns constructor.
-     * @param QrService $qrService
-     * @param FnsService $fnsService
+     *
+     * @param  QrService  $qrService
+     * @param  FnsService  $fnsService
      */
-    public function __construct( QrService $qrService, FnsService $fnsService )
+    public function __construct(QrService $qrService, FnsService $fnsService)
     {
         $this->qrService = $qrService;
         $this->fnsService = $fnsService;
@@ -36,13 +39,14 @@ class Fns
 
     /**
      * Get info about a check,
+     *
      * @param $checkFile
      * @return Collection|bool
      * @throws \Exception
      */
-    public function getInfoByCheck( $checkFile )
+    public function getInfoByCheck($checkFile)
     {
-        if ( $this->qrService->getCheck($checkFile) ) {
+        if ($this->qrService->getCheck($checkFile)) {
             return collect($this->qrService->getCheck($checkFile));
         }
 
@@ -52,12 +56,19 @@ class Fns
 
     /**
      * Return Collection if check recognized, if check not recognized return false
+     *
      * @param $checkFile
      * @return Collection|bool
      * @throws \Exception
      */
-    public function getAllCheckItems( $checkFile )
+    public function getAllCheckItems($checkFile)
     {
         return $this->fnsService->getCheckItems($this->qrService->getCheck($checkFile));
+    }
+
+    public function getCheckByQr($qr)
+    {
+        $data = FnsCheckHelper::fromQRCode($qr);
+        return $this->fnsService->getCheckItems($data);
     }
 }
